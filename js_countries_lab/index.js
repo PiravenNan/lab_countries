5bf7f77e7b11a8620f24669c78bb35a7f980e026
Piraven Nandakumaran <Xothspath@gmail.com>
2023-11-28 18:50:01 +0000

    User can enter country, press button to submit and have the country's name logged in the console

--- js_countries_lab/index.js
+++ js_countries_lab/index.js
@@ -1,7 +1,24 @@
+const list = document.querySelector("#list");
+const submittedCountry = document.querySelector("#submittedCountry");
 const getCountryByName = async(countryName)=>{
     const response = await fetch("https://restcountries.com/v3.1/name/".concat(countryName)); 
+    const data = await response.json();
+    return data;
 
-    console.log(response);
+    //console.log(response);
 }
 
-getCountryByName("Australia")
+const button = document.querySelector("#enter");
+
+document.querySelector("#enter").addEventListener("click", async function (event) {
+
+    event.preventDefault();
+    //ist.splice(0,list.length)
+
+    const countryData = await getCountryByName(submittedCountry.value);
+    const countryName = countryData[0].name.common;
+    console.log(countryName);
+
+   //name, capital, languages and population
+});
+
