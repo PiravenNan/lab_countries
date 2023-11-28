1fbc900245af0f992525d0832ba35609b380771c
Piraven Nandakumaran <Xothspath@gmail.com>
2023-11-28 23:14:38 +0000

    Can get a country's pop, capital(s), language(s), and offical name - displays this infomation as a list on the webpage

--- js_countries_lab/index.js
+++ js_countries_lab/index.js
@@ -13,12 +13,54 @@ const button = document.querySelector("#enter");
 document.querySelector("#enter").addEventListener("click", async function (event) {
 
     event.preventDefault();
-    //ist.splice(0,list.length)
 
+    list.innerHTML = "";
+    
     const countryData = await getCountryByName(submittedCountry.value);
-    const countryName = countryData[0].name.common;
-    console.log(countryName);
 
-   //name, capital, languages and population
+    const countryName = document.createElement("li");
+    countryName.innerText = "Country name: ".concat(countryData[0].name.official);
+
+    const countryCapital = document.createElement("li");
+    countryCapital.innerText = "Country Capital(s): "
+
+    const capitalList = document.createElement("ul");
+    capitalList.classList.add("capital-list");
+
+    countryData[0].capital.forEach(capitals => {
+        const capitalListItem = document.createElement("li");
+        capitalListItem.innerText = capitals;
+        capitalList.appendChild(capitalListItem);
+    });
+
+    countryCapital.appendChild(capitalList);
+    //
+    
+    //
+    const countryLanguage = document.createElement("li");
+    countryLanguage.innerText = "Country language(s): "
+
+    const languageList = document.createElement("ul");
+    languageList.classList.add("language-list");
+
+    Object.keys(countryData[0].languages).forEach(langaugeKey => {
+        const languageListItem = document.createElement("li");
+        languageListItem.innerText = countryData[0].languages[langaugeKey];
+        languageList.appendChild(languageListItem);
+    });
+
+    countryLanguage.appendChild(languageList);
+    //
+
+    const countryPopulation = document.createElement("li");
+    countryPopulation.innerText = "Country Population: ".concat(countryData[0].population);
+    
+
+    list.appendChild(countryName);
+    list.appendChild(countryCapital);
+    list.appendChild(countryLanguage);
+    list.appendChild(countryPopulation);
+
+   
 });
 
