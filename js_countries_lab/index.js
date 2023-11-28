b8898d444b51c4430be88241aa8bd86ef8ac5568
Piraven Nandakumaran <Xothspath@gmail.com>
2023-11-28 17:59:29 +0000

    Can fetch single country

--- js_countries_lab/index.js
+++ js_countries_lab/index.js
@@ -1,1 +1,7 @@
-console.log("Hello");
+const getCountryByName = async(countryName)=>{
+    const response = await fetch("https://restcountries.com/v3.1/name/".concat(countryName)); 
+
+    console.log(response);
+}
+
+getCountryByName("Australia")
